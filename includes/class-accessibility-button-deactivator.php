6aecae5e6d06819f6416208540c390d6deda0816
Amit Rahav <amit.r.89@gmail.com>
2019-06-03 19:12:05 +0300

    init plugin stracture

---
+++ includes/class-accessibility-button-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://the-two.co/
+ * @since      1.0.0
+ *
+ * @package    Accessibility_Button
+ * @subpackage Accessibility_Button/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Accessibility_Button
+ * @subpackage Accessibility_Button/includes
+ * @author     TheTwo <amit.r.89@gmail.com>
+ */
+class Accessibility_Button_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
